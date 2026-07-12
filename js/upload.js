54d23e826064e06c0459ad7999d6849b258c29ff
reyclouddev-ops <reyclouddev@gmail.com>
2026-07-12 15:40:58 +0700

    Update upload.js

--- js/upload.js
+++ js/upload.js
@@ -1,8 +1,48 @@
 const Upload = {
 
-    initSlot(slot) {
+    bind(slot) {
 
-        console.log("Upload Ready");
+        const input =
+            slot.querySelector("input");
+
+        const img =
+            slot.querySelector(".preview-image");
+
+        const wrapper =
+            slot.querySelector(".photo-wrapper");
+
+        const upload =
+            slot.querySelector(".upload-box");
+
+        slot.addEventListener("click", () => {
+
+            input.click();
+
+        });
+
+        input.addEventListener("change", e => {
+
+            const file = e.target.files[0];
+
+            if (!file) return;
+
+            const reader = new FileReader();
+
+            reader.onload = () => {
+
+                img.src = reader.result;
+
+                wrapper.style.display = "block";
+
+                upload.style.display = "none";
+
+                Editor.enable(img);
+
+            };
+
+            reader.readAsDataURL(file);
+
+        });
 
     }
 
