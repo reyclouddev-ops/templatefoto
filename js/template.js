37895aa34c74d3a7fdc71fa8508080f9725a6ca0
reyclouddev-ops <reyclouddev@gmail.com>
2026-07-12 15:40:16 +0700

    Update template.js

--- js/template.js
+++ js/template.js
@@ -2,7 +2,76 @@ const Template = {
 
     create(total) {
 
-        console.log("Create Template :", total);
+        const preview = App.preview;
+
+        preview.innerHTML = "";
+
+        let columns = 2;
+
+        switch (total) {
+
+            case 4:
+                columns = 2;
+                break;
+
+            case 8:
+                columns = 2;
+                break;
+
+            case 12:
+                columns = 3;
+                break;
+
+        }
+
+        preview.style.gridTemplateColumns =
+            `repeat(${columns},1fr)`;
+
+        for (let i = 1; i <= total; i++) {
+
+            const slot = this.createSlot(i);
+
+            preview.appendChild(slot);
+
+        }
+
+    },
+
+    createSlot(index) {
+
+        const slot = create("div", "slot");
+
+        slot.innerHTML = `
+
+<input type="file" hidden accept="image/*">
+
+<div class="photo-wrapper">
+
+<img class="preview-image">
+
+<div class="zoom-controls">
+
+<button class="zoom-in">+</button>
+
+<button class="zoom-out">−</button>
+
+</div>
+
+</div>
+
+<div class="upload-box">
+
+<i class="fa-solid fa-cloud-arrow-up"></i>
+
+<span>Upload Foto ${index}</span>
+
+</div>
+
+`;
+
+        Upload.bind(slot);
+
+        return slot;
 
     }
 
